84d143bf237e63ab3b2220fd6fbf3a1aa37e42b0
Quin-shi-huan <phelipee06@gmail.com>
2025-06-18 13:18:08 -0300

    fix: Adjusting BalanceEditor punctuation sign

--- src/controllers/balance_editor.py
+++ src/controllers/balance_editor.py
@@ -3,7 +3,7 @@ from .interfaces.balance_editor import BalanceEditorInterface
 
 class BalanceEditor(BalanceEditorInterface):
     def __init__(self, user_repository: UserRepositoryInterface) -> None:
-        self.__user_repository: user_repository
+        self.__user_repository = user_repository
 
     def edit(self, user_id: int, new_balance: float) -> dict:
         self.__user_repository.edit_balance(user_id, new_balance)
